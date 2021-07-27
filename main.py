e938ea716ea6919ff3bfe180b991baa6554d47f8
Shashank Tiwari <63389023+MrShashankTiwari@users.noreply.github.com>
2021-07-27 08:20:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ user_cards = []
 computer_cards = []
 
 def ge21():
+  
   if sum(user_cards) == 21:
     return 0
   elif sum(computer_cards) == 21:
@@ -22,6 +23,7 @@ def ge21():
     return 100
 
 def ge212():
+  
   if sum(user_cards) == 21:
     print("You win 😁")
   elif sum(computer_cards) == 21:
@@ -30,6 +32,7 @@ def ge212():
     print("You Lose ☹")
 
 def ace():
+  
   for card in user_cards:
     if card == 11 and sum(user_cards) > 21:
       user_cards.remove(11)
@@ -41,6 +44,7 @@ def ace():
       computer_cards.append(1)
 
 def yes():
+  
   b3 = deal_card()
   user_cards.append(b3)
   ace()
@@ -49,6 +53,7 @@ def yes():
   b4 = deal_card()
   computer_cards.append(b4)
   ace()
+  
   if sum(user_cards) >= 21 or sum(computer_cards) >= 21:
     ge212()
   else:
@@ -57,12 +62,21 @@ def yes():
     yorn(x)
 
 def no():
+  
   print(f"Your final hand: {user_cards}, final score: {sum(user_cards)}")
+  
+  while(sum(computer_cards) <= 17):
+    b5 = deal_card()
+    computer_cards.append(b5)
+
   print(f"Computer's final hand: {computer_cards}, final score: {sum(computer_cards)}")
+  
   if sum(user_cards) > sum(computer_cards):
     print("You win 😁")
-  elif sum(user_cards) < sum(computer_cards):
+  elif sum(user_cards) < sum(computer_cards) and sum(computer_cards) <= 21:
     print("You Lose ☹")
+  elif sum(computer_cards) > 21:
+    print("You win 😁")
   else :
     print("Draw")
 
